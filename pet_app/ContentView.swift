8f537da7697c39d38f9fb66e885b7e1cb143ee6c
AnyaH0831 <anyahuang0831@gmail.com>
2026-03-07 16:09:28 -0500

    Initial commit

--- pet_app/ContentView.swift
+++ pet_app/ContentView.swift
@@ -1,61 +1,24 @@
 //
 //  ContentView.swift
-//  pet_app
+//
 //
 //  Created by Anya Huang on 2026-03-07.
 //
 
 import SwiftUI
-import SwiftData
 
-struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
-    var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+struct ContentView: View{
+    var body: some View{
+        ZStack{
+            Color.black.ignoresSafeArea()
+            
+            Circle()
+                .fill(Color.gray)
+                .frame(width: 120, height: 120)
+                  
         }
     }
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
-}
+
+
